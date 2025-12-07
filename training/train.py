bf38e63de359d5a0e88bff52175017efa0c05cc1
Mukama Francois <mk.frmh3@gmail.com>
2025-12-08 00:05:40 +0200

    .ipynb

--- training/train.py
+++ training/train.py
@@ -19,7 +19,7 @@ else:
 
 # Import Models (No Try/Except -> We want to see the error!)
 print("🔄 Importing Wav2Lip models...")
-from models import Wav2Lip, Wav2Lip_Disc_Qual, SyncNet_color
+from models import Wav2Lip, Wav2Lip_Disc_qual, SyncNet_color
 print("✅ Import successful.")
 
 # Import local modules
@@ -45,7 +45,7 @@ def train():
     
     # 1. Models
     model = Wav2Lip().to(DEVICE)
-    disc = Wav2Lip_Disc_Qual().to(DEVICE)
+    disc = Wav2Lip_Disc_qual().to(DEVICE)
     syncnet = SyncNet_color().to(DEVICE)
     
     # Load Experts
